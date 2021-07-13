19fe7f7042972e3ed96d420e2a0900fa103c10a1
Max Zheltikov <maxcruer@gmail.com>
2021-07-13 12:35:39 +0300

    marked InvariantException as deprecated

--- src/InvariantException.php
+++ src/InvariantException.php
@@ -7,6 +7,7 @@ use Exception;
 /**
  * Class InvariantException
  * @package Zheltikov\Invariant
+ * @deprecated Use `InvariantException` from `zheltikov/php-exceptions`
  */
 class InvariantException extends Exception
 {
